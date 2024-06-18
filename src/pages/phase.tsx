a13b8b88b80279dbdec93a6f2d922e38730b4053
Soumya Ranjan <soumyaranjan@rayvector.com>
2024-06-18 17:53:16 +0530

    Updated demo video link

--- src/pages/phase.tsx
+++ src/pages/phase.tsx
@@ -105,7 +105,7 @@ const Phase = () => {
             <div className="flex justify-between items-center gap-4 mb-2">
               <h2 className="text-lg capitalize font-bold">Project Demo</h2>
             </div>
-            <VideoPlayer src="https://utfs.io/f/ec4954a7-38a6-43db-8fd8-381c15f2bd7c-18qyia.m4v" />
+            <VideoPlayer src="https://utfs.io/f/ad183797-0fb6-4879-b90c-260b7f71daca-e0m3b2.m4v" />
           </div>
           <div className="space-y-4">
             <h2 className="text-lg capitalize font-bold">
